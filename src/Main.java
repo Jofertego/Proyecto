f6edf9aebcdb7fe00d62b03b341e313ee2589a7b
Johan <fernandojo27oq@gmail.com>
2026-02-19 14:51:34 -0500

    Correccion

--- src/Main.java
+++ src/Main.java
@@ -6,11 +6,9 @@ public class Main {
         System.out.println("Johan Fernando Oyaga Quintero");
         System.out.println("Mis tres hobbies son: - Tocar trompeta - Jugar videojuegos - Hacer multiples deportes");
         System.out.println("Mi deporte favorito es el Voleibol");
-        System.out.println("PRUEBA");
-    }
-}
+        System.out.println("-----------------------------------------------------------------------------------------");
 
         System.out.println(" Duvan Felipe Suarez Parada ");
         System.out.println( " senderismo , jugar billar y jugar videojuegos ");
         System.out.println("practico futbol");
-
+ }}
