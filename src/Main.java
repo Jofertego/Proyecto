4b28c4a3fc741316d5dce36b7eee684024131c7c
Duvan Felipe Suarez <suarezduvan268@gmailcom.>
2026-02-19 14:17:30 -0500

    hobbies Felipe

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-
-    }
-}
+        System.out.println(" Duvan Felipe Suarez Parada ");
+        System.out.println( " senderismo , jugar billar y jugar videojuegos ");
+        System.out.println("practico futbol");
+}}
